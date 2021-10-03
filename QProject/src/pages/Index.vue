ba5c2a9cf143f30f5aa1c31a022f7ca564ac675b
firestormXCV <54678091+firestormXCV@users.noreply.github.com>
2021-10-03 23:10:02 +0200

    bb

--- QProject/src/pages/Index.vue
+++ QProject/src/pages/Index.vue
@@ -1,23 +1,23 @@
 <template>
+
+  <video v-on:ended="startPage" autoplay muted class="video">
+
+    <source src="test.mp4"
+            type="video/webm">
+
+    <source src="test.mp4"
+            type="video/mp4">
+
+    Sorry, your browser doesn't support embedded videos.
+</video>
+
+
+
   <div>
 
-    
-    <div class="q-pa-md bg-image">
-      <q-parallax :height="150">
-        <template v-slot:media>
-          <video width="720" height="440" poster="https://cdn.quasar.dev/img/polina.jpg" autoplay loop muted>
-            <source type="video/webm" src="https://cdn.quasar.dev/img/polina.webm">
-            <source type="video/mp4" src="https://cdn.quasar.dev/img/polina.mp4">
-          </video>
-        </template>
-
-        <h3 class="text-white">Video</h3>
-      </q-parallax>
-    </div>
-
-
     <q-carousel
-        
+        id="carrousel"
+        ref="carrousel"
         v-model="slide"
         transition-prev="scale"
         transition-next="scale"
@@ -33,7 +33,9 @@
         @mouseleave="autoplay = true"
         height="500px"
         class="text-white shadow-1 rounded-borders q-mt-md"
-        style="background-color: #1d76af;">
+        style="background-color: #1d76af;
+              transition-delay: 10s;
+              transition: width 2s, height 2s, background-color 2s, transform 2s;">
 
 
 
@@ -66,16 +68,45 @@
 </template>
 
 <script>
+import { onMounted } from 'vue'
 import { ref } from 'vue'
 
 export default {
   setup () {
+
+onMounted(() => {
+     const carrousel = document.querySelector('#carrousel');
+    carrousel.style.display="none";
+}) 
+
+
     return {
       slide: ref('style'),
       autoplay: ref(true),
       lorem: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque voluptatem totam, architecto cupiditate officia rerum, error dignissimos praesentium libero ab nemo.'
     }
   }
+,
+methods: { startPage:function() {
+  const carrousel = document.querySelector('#carrousel');
+  //console.log(carrousel);
+  carrousel.style.display="block";
+  window.setTimeout(3000000)
+  }
 }
 
+
+}
+
+
+
 </script>
+
+<style>
+.video {
+        position: absolute;
+        z-index: -1;
+        height: 100%;
+        width: 100%;
+      }
+</style>
