5231557a6e72697161bd384ee7fff2fc121ec187
firestormXCV <54678091+firestormXCV@users.noreply.github.com>
2021-10-05 11:19:43 +0200

    h

--- QProject/src/pages/Index.vue
+++ QProject/src/pages/Index.vue
@@ -1,6 +1,6 @@
 <template>
 
-  <video v-on:ended="startPage" autoplay muted class="video">
+  <video v-on:ended="visible = !visible" autoplay muted class="video">
 
     <source src="test.mp4"
             type="video/webm">
@@ -9,62 +9,63 @@
             type="video/mp4">
 
     Sorry, your browser doesn't support embedded videos.
-</video>
+  </video>
+
+  <transition name="fade">
+    <div v-if="visible" > 
+
+      <q-carousel
+  
+        
+          id="carrousel"
+          ref="carrousel"
+          v-model="slide"
+          transition-prev="scale"
+          transition-next="scale"
+          swipeable
+          animated
+          control-color="white"
+          navigation
+          padding
+          arrows
+          infinite
+          :autoplay="autoplay"
+          @mouseenter="autoplay = false"
+          @mouseleave="autoplay = true"
+          height="500px"
+          class="text-white shadow-1 rounded-borders q-mt-md"
+          style="background-color: #1d76af;
+                ">
 
 
 
-  <div>
-
-    <q-carousel
-        id="carrousel"
-        ref="carrousel"
-        v-model="slide"
-        transition-prev="scale"
-        transition-next="scale"
-        swipeable
-        animated
-        control-color="white"
-        navigation
-        padding
-        arrows
-        infinite
-        :autoplay="autoplay"
-        @mouseenter="autoplay = false"
-        @mouseleave="autoplay = true"
-        height="500px"
-        class="text-white shadow-1 rounded-borders q-mt-md"
-        style="background-color: #1d76af;
-              transition-delay: 10s;
-              transition: width 2s, height 2s, background-color 2s, transform 2s;">
-
-
-
-        <q-carousel-slide name="style" class="column no-wrap flex-center">
-          <q-icon name="style" size="56px" />
-          <div class="q-mt-md text-center">
-            {{ lorem }}
-          </div>
-        </q-carousel-slide>
-        <q-carousel-slide name="tv" class="column no-wrap flex-center">
-          <q-icon name="live_tv" size="56px" />
-          <div class="q-mt-md text-center">
-            {{ lorem }}
-          </div>
-        </q-carousel-slide>
-        <q-carousel-slide name="layers" class="column no-wrap flex-center">
-          <q-icon name="layers" size="56px" />
-          <div class="q-mt-md text-center">
-            {{ lorem }}
-          </div>
-        </q-carousel-slide>
-        <q-carousel-slide name="map" class="column no-wrap flex-center">
-          <q-icon name="terrain" size="56px" />
-          <div class="q-mt-md text-center">
-            {{ lorem }}
-          </div>
-        </q-carousel-slide>
-      </q-carousel>
-  </div>
+          <q-carousel-slide name="style" class="column no-wrap flex-center">
+            <q-icon name="style" size="56px" />
+            <div class="q-mt-md text-center">
+              {{ lorem }}
+            </div>
+          </q-carousel-slide>
+          <q-carousel-slide name="tv" class="column no-wrap flex-center">
+            <q-icon name="live_tv" size="56px" />
+            <div class="q-mt-md text-center">
+              {{ lorem }}
+            </div>
+          </q-carousel-slide>
+          <q-carousel-slide name="layers" class="column no-wrap flex-center">
+            <q-icon name="layers" size="56px" />
+            <div class="q-mt-md text-center">
+              {{ lorem }}
+            </div>
+          </q-carousel-slide>
+          <q-carousel-slide name="map" class="column no-wrap flex-center">
+            <q-icon name="terrain" size="56px" />
+            <div class="q-mt-md text-center">
+              {{ lorem }}
+            </div>
+          </q-carousel-slide>
+        </q-carousel>
+    </div>
+  </transition>
 </template>
 
 <script>
@@ -72,15 +73,17 @@ import { onMounted } from 'vue'
 import { ref } from 'vue'
 
 export default {
+
   setup () {
 
 onMounted(() => {
      const carrousel = document.querySelector('#carrousel');
-    carrousel.style.display="none";
+    
 }) 
 
 
     return {
+      visible: ref(false),
       slide: ref('style'),
       autoplay: ref(true),
       lorem: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque voluptatem totam, architecto cupiditate officia rerum, error dignissimos praesentium libero ab nemo.'
@@ -90,8 +93,8 @@ onMounted(() => {
 methods: { startPage:function() {
   const carrousel = document.querySelector('#carrousel');
   //console.log(carrousel);
-  carrousel.style.display="block";
-  window.setTimeout(3000000)
+  carrousel.style.opacity=1;
+  carrousel
   }
 }
 
@@ -108,5 +111,21 @@ methods: { startPage:function() {
         z-index: -1;
         height: 100%;
         width: 100%;
-      }
+}
+</style>
+
+<style scoped>
+.fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  transform: translateY(100px);
+  opacity: 0;
+}
 </style>
